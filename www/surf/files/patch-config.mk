1c3d3da911e11d759f0169310d78444e0c899033
Robert Clausecker <fuz@FreeBSD.org>
2024-05-24 20:41:08 +0200

    www/surf: update webkit2gtk3 API version to 4.1
     - turn static REINPLACE_CMD into patch as per policy
    
    Pull Request:	https://github.com/freebsd/freebsd-ports/pull/266

---
+++ www/surf/files/patch-config.mk
@@ -0,0 +1,34 @@
+--- config.mk.orig	2021-05-09 22:34:33 UTC
++++ config.mk
+@@ -4,7 +4,7 @@ VERSION = 2.1
+ # Customize below to fit your system
+ 
+ # paths
+-PREFIX = /usr/local
++PREFIX ?= /usr/local
+ MANPREFIX = $(PREFIX)/share/man
+ LIBPREFIX = $(PREFIX)/lib
+ LIBDIR = $(LIBPREFIX)/surf
+@@ -12,17 +12,17 @@ X11LIB = `pkg-config --libs x11`
+ X11INC = `pkg-config --cflags x11`
+ X11LIB = `pkg-config --libs x11`
+ 
+-GTKINC = `pkg-config --cflags gtk+-3.0 gcr-3 webkit2gtk-4.0`
+-GTKLIB = `pkg-config --libs gtk+-3.0 gcr-3 webkit2gtk-4.0`
+-WEBEXTINC = `pkg-config --cflags webkit2gtk-4.0 webkit2gtk-web-extension-4.0 gio-2.0`
+-WEBEXTLIBS = `pkg-config --libs webkit2gtk-4.0 webkit2gtk-web-extension-4.0 gio-2.0`
++GTKINC ?= `pkg-config --cflags gtk+-3.0 gcr-3 webkit2gtk-4.1`
++GTKLIB ?= `pkg-config --libs gtk+-3.0 gcr-3 webkit2gtk-4.1`
++WEBEXTINC = `pkg-config --cflags webkit2gtk-4.1 webkit2gtk-web-extension-4.1 gio-2.0`
++WEBEXTLIBS = `pkg-config --libs webkit2gtk-4.1 webkit2gtk-web-extension-4.1 gio-2.0`
+ 
+ # includes and libs
+ INCS = $(X11INC) $(GTKINC)
+ LIBS = $(X11LIB) $(GTKLIB) -lgthread-2.0
+ 
+ # flags
+-CPPFLAGS = -DVERSION=\"$(VERSION)\" -DGCR_API_SUBJECT_TO_CHANGE \
++CPPFLAGS += -DVERSION=\"$(VERSION)\" -DGCR_API_SUBJECT_TO_CHANGE \
+            -DLIBPREFIX=\"$(LIBPREFIX)\" -DWEBEXTDIR=\"$(LIBDIR)\" \
+            -D_DEFAULT_SOURCE
+ SURFCFLAGS = -fPIC $(INCS) $(CPPFLAGS)
