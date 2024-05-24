b37139f33ee88aeae531e05cbd39d45f90ec8677
Robert Clausecker <fuz@FreeBSD.org>
2024-05-24 20:42:48 +0200

    devel/juce: update webkit2gtk3 API version to 4.1
     - remove duplicate libsoup LIB_DEPENDS from DEMOS option, the library
       is already pulled in unconditionally
    
    Pull Request:	https://github.com/freebsd/freebsd-ports/pull/266

---
+++ devel/juce/files/patch-extras_Projucer_Source_ProjectSaving_jucer__ProjectExporter.cpp
@@ -0,0 +1,11 @@
+--- extras/Projucer/Source/ProjectSaving/jucer_ProjectExporter.cpp.orig	2024-05-24 21:26:57 UTC
++++ extras/Projucer/Source/ProjectSaving/jucer_ProjectExporter.cpp
+@@ -607,7 +607,7 @@ StringArray ProjectExporter::getLinuxPackages (Package
+ 
+     if (isWebBrowserComponentEnabled (project) && type == PackageDependencyType::compile)
+     {
+-        packages.add ("webkit2gtk-4.0");
++        packages.add ("webkit2gtk-4.1");
+         packages.add ("gtk+-x11-3.0");
+     }
+ 
